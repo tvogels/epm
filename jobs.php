ac5007ba792312dedb52dd2ab12314a39cb50608
Thijs Vogels <t.vogels@me.com>
2014-09-11 17:31:14 +0200

    Shadow

--- jobs.php
+++ jobs.php
@@ -23,11 +23,38 @@
             <p class="lead">Wir möchten unser Team verstärken und freuen uns auf Ihre aussagekräftige Bewerbung. Folgende Positionen möchten wir besetzen:</p>
 
             <hr>
-            <h4>Senior Web-Engineer</h4>
-            <p>Nam arcu urna, fermentum non, lacinia ut, commodo id, nisi. Suspendisse odio urna, mollis vitae, pretium in, porta in, ligula. Fusce neque felis, posuere quis, ultrices eu, mollis a, turpis. Praesent sem urna, semper quis, sollicitudin vel, commodo sit amet, enim. Fusce semper sem vel ipsum. Vivamus et massa. Mauris pretium blandit lorem.</p>
+            <h4>Senior Consultant Automotive (m/w)</h4>
+            <p>Unser Automotive-Team berät internationale Unternehmen in der Automobil-Branche.</p>
+            <p>Wir unterstützen unsere Kunden bei der Einführung neuer Produkte, der erfolgreichen Etablierung neuer Geschäftsfelder, der Optimierung bestehender Produktionsbetriebe und der Restrukturierung kompletter Unternehmensbereiche.</p>
+            <p>Für unser wachsendes Team suchen wir kompetente Verstärkung.</p>
+            <p>Nach einem sehr guten Abschluss an einer Universität oder einer internationalen Hochschule haben Sie (m/w) bereits mehrere Jahre in der Industrie und/oder als Berater Erfahrung gesammelt. Sie haben sehr gute analytische Fähigkeiten und sind in der Lage, innovative und praktikable Problemlösungen zu erarbeiten. Sie haben fundiertes Wissen über die Prinzipien von Lean-Production und Lean-Management und sind in der Lage, Projekt- und Teamverantwortung zu übernehmen. Sie sind flexibel  und teamorientiert, haben ein sicheres Auftreten im internationalen Umfeld und verfügen über sehr gute Englischkenntnisse.</p>
+            <p>Sind Sie bereit für eine neue Herausforderung? Dann schicken Sie uns bitte Ihre aussagekräftige Bewerbung per Email an bewerbung@ep-m.de oder per Post.</p>
+
+
+
             <hr>
-            <h4>Big-Data Analyst</h4>
-            <p>Donec posuere lectus ut sapien. Fusce mollis dui adipiscing sapien. Sed condimentum pede quis ipsum. Donec mauris ligula, cursus eget, hendrerit ut, tempor nec, urna. Curabitur fringilla nisl et nibh. Aliquam ut ligula vel velit dignissim porttitor. Suspendisse eget mi.</p>
+            <h4>Consultant Automotive (m/w)</h4>
+            <p>Unser Automotive-Team berät internationale Unternehmen in der Automobil-Branche.</p>
+            <p>Wir unterstützen unsere Kunden bei der Einführung neuer Produkte, der erfolgreichen Etablierung neuer Geschäftsfelder, der Optimierung bestehender Produktionsbetriebe und der Restrukturierung kompletter Unternehmensbereiche.</p>
+            <p>Für unser wachsendes Team suchen wir kompetente Verstärkung.</p>
+            <p>Sie haben einen sehr guten Abschluss an einer Universität oder Hochschule im Bereich Wirtschaft und/oder Technik. Sie sind in der Lage, komplexe Prozesse strukturiert zu analysieren und Ergebnisse aussagekräftig aufzubereiten. Neben der gesamten Office-Suite haben Sie auch Erfahrung gesammelt mit Methoden der Prozess- und Fehleranalyse. Einer Ihrer universitären Schwerpunkte war entweder Logistik, Qualitätsmanagement oder Prozessoptimierung. Sie haben sehr gute Englischkenntnisse und ein sicheres Auftreten. Sie sind teamfähig und haben einen sehr hohen Qualitätsanspruch an Ihre Arbeit.</p>
+            <p>Haben wir Ihr Interesse geweckt? Dann schicken Sie uns bitte Ihre aussagekräftige Bewerbung per Email an bewerbung@ep-m.de oder per Post.</p>
+
+            
+            <hr>
+            <h4>Consultant (m/w) allgemein</h4>
+            <p>Die E-P-M GmbH ist ein stark wachsendes, auf Produktionsbetriebe spezialisiertes Beratungsunternehmen. Gemeinsam mit unseren Kunden entwickeln wir Strategien und Konzepte, die das Angebot unserer Kunden optimal auf die Bedürfnisse des Marktes abstimmt. Um die Wertschöpfung zu optimieren, erarbeiten wir Maßnahmen zur kontinuierlichen Verbesserung von Produktions-Prozessen und zur Kostenoptimierung. Durch eine aktive Unterstützung bei der Umsetzung von Optimierungs-Maßnahmen helfen wir unseren Kunden, schnell die gewünschten Ergebnisse zu erreichen.</p>
+            <p>Wir suchen zur Unterstützung unseres Teams Consultants (m/w) in allen Ebenen, vom Studienabsolventen bis hin zum Berater mit mehrjähriger Berufserfahrung. Sie haben einen sehr guten Ausbildungsabschluss und verfügen über sehr gute englische Sprachkenntnisse. Sie sind analytisch, verfügen über unternehmerisches Denken und haben Erfahrung in der Teamarbeit. </p>
+            <p>Haben wir Ihr Interesse geweckt? Dann schicken Sie uns bitte Ihre aussagekräftige Bewerbung per Email an bewerbung@ep-m.de oder per Post.</p>
+
+
+            <hr>
+            <h4>Senior Consultant Restructuring (m/w)</h4>
+            <p>Unser Restrukturierungs- und Turn-Around-Team berät und unterstützt Unternehmen bei umfassenden Restrukturierungen. Dabei reichen die Aufgabenstellungen von strategischen über operative bis hin zu finanziellen Fragestellungen. Gemeinsam mit dem Kunden erarbeiten wir auch umfassende Sanierungskonzepte, die wir gemeinsam mit dem Kunden umsetzen. Oft müssen Ergebnisse schnell erreicht werden, weshalb Pragmatismus, Umsetzungsstärke und Erfahrung im Umgang mit Unternehmensvertretern wie auch Stakeholdern gefordert sind.</p>
+            <p>Sie (m/w) haben Ihr Studium mit sehr gutem Ergebnis abgeschlossen und verfügen über mehrjährige Erfahrung in der Optimierung und Restrukturierung von Unternehmen. Sie verfügen über ein Repertoire an unterschiedlichen Methoden zur nachhaltigen Effizienz- und Profitabilitätssteigerung und sind in der Lage, mit dessen Hilfe Maßnahmen zu definieren, die zu schnellen und nachhaltigen Ergebnissen führen. Sie sind zudem in der Lage, bei der Umsetzung der mit dem Kunden erarbeiteten Maßnahmen zu unterstützen, um den Erfolg zu sichern. Sie verfügen über strategisches und unternehmerisches Denken, sind verhandlungssicher und haben ein sicheres Auftreten. Sehr gute Englischkenntnisse sind für Sie selbstverständlich.</p>
+            <p>Haben wir Ihr Interesse geweckt? Dann schicken Sie uns bitte Ihre aussagekräftige Bewerbung per Email an bewerbung@ep-m.de oder per Post.</p>
+
+
             <hr>
           </div>
           <div class="col-sm-4">
