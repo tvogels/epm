f2343e567c61c0bc847c55a973d8475c3a5c6ebc
Thijs Vogels <t.vogels@me.com>
2014-09-11 11:00:22 +0200

    Add leadership page

---
+++ leadership.php
@@ -0,0 +1,104 @@
+<?php require_once('bootstrap.php'); ?>
+<?php epm_header(array(
+  'active' => 'leadership',
+  'page-title' => 'Leadership'
+)); ?>
+      <div class="global-banner banner-small" style="background-image:url(img/car-bg.jpg);">
+        <div class="container banner-container">
+          <div class="row">
+            <div class="col-sm-6 col-sm-offset-6 banner-content">
+              <p>What can we do for</p>
+              <p>your business?</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container header-bottom">
+      </div>
+    </header>
+
+    <div class="global-body management">
+      <div class="container">
+        <h1>Management</h1>
+        <div class="row">
+          <div class="col-md-1 col-xs-2">
+            <div class="thumbnail">
+              <img src="/img/bernhard-ranftl.jpg" alt="Bernhard Ranftl">
+            </div>
+          </div>
+          <div class="col-md-3 col-xs-10">
+            <h3>Bernhard Ranftl</h3>
+            <ul class="list-unstyled list-spacious">
+              <li><span style="color:#888;">Geb.</span> 24.8.1962</li>
+              <li>Gründer der EPM. Ausbildung zum Modellbau-Meister mit Fachrichtung Automotive, aufbauend darauf BWL-Studium. </li>
+              <li>Seit 30 Jahren in der Automobilindustrie tätig. Zunächst Geschäftsführer mittelständischer Fertigungsbetriebe, später dann mit der EPM eine Consulting Gesellschaft für Restrukturierungs- und Turnaround-Management Projekte aufgebaut. </li>
+            </ul>
+          </div>
+          <div class="col-md-1 col-xs-2">
+            <div class="thumbnail">
+              <img src="/img/dietmar-wolf.jpg" alt="Dietmar Wolf">
+            </div>
+          </div>
+          <div class="col-md-3 col-xs-10">
+            <h3>Dietmar Wolf</h3>
+            <ul class="list-unstyled list-spacious">
+              <li><span style="color:#888;">Geb.</span>  21.5.1957</li>
+              <li>Nach seiner Ausbildung zum Elektronik-Ingenieur war Herr Wolf in großen börsennotierten Unternehmen der Automobilindustrie  in leitenden Positionen für die Bereiche Einkauf und Logistik verantwortlich. Seit 2004 ist Herr Wolf Senior Partner der E-P-M GmbH.</li>
+            </ul>
+          </div>
+          <div class="col-md-1 col-xs-2">
+            <div class="thumbnail">
+              <img src="/img/roy-von-der-locht.jpg" alt="Roy von der Locht">
+            </div>
+          </div>
+          <div class="col-md-3 col-xs-10">
+            <h3>Roy von der Locht</h3>
+            <ul class="list-unstyled list-spacious">
+              <li><span style="color:#888;">Geb.</span> 7.3.1964</li>
+              <li>Prädikatsexamen Informatik an der TU München. Danach Aufbau und Leitung mehrerer Unternehmen, unter anderem als CEO zweier börsennotierter AGs. Seit 2012 als Senior Partner in der E-P-M GmbH.</li>
+            </ul>
+          </div>
+        </div>
+
+        <h1>Beirat</h1>
+        <div class="row">
+          <div class="col-md-1 col-xs-2">
+            <div class="thumbnail">
+              <img src="/img/bernhard-ranftl.jpg" alt="Bernhard Ranftl">
+            </div>
+          </div>
+          <div class="col-md-3 col-xs-10">
+            <h3>Bernhard Ranftl</h3>
+            <ul class="list-unstyled list-spacious">
+              <li><span style="color:#888;">Geb.</span> 24.8.1962</li>
+              <li>Seit 30 Jahren in der Automobilindustrie tätig. Zunächst Geschäftsführer mittelständischer Fertigungsbetriebe, später dann mit der EPM eine Consulting Gesellschaft für Restrukturierungs- und Turnaround-Management Projekte aufgebaut. </li>
+            </ul>
+          </div>
+          <div class="col-md-1 col-xs-2">
+            <div class="thumbnail">
+              <img src="/img/dietmar-wolf.jpg" alt="Dietmar Wolf">
+            </div>
+          </div>
+          <div class="col-md-3 col-xs-10">
+            <h3>Dietmar Wolf</h3>
+            <ul class="list-unstyled list-spacious">
+              <li><span style="color:#888;">Geb.</span>  21.5.1957</li>
+              <li>Nach seiner Ausbildung zum Elektronik-Ingenieur war Herr Wolf in großen börsennotierten Unternehmen der Automobilindustrie  in leitenden Positionen für die Bereiche Einkauf und Logistik verantwortlich. Seit 2004 ist Herr Wolf Senior Partner der E-P-M GmbH.</li>
+            </ul>
+          </div>
+          <div class="col-md-1 col-xs-2">
+            <div class="thumbnail">
+              <img src="/img/roy-von-der-locht.jpg" alt="Roy von der Locht">
+            </div>
+          </div>
+          <div class="col-md-3 col-xs-10">
+            <h3>Roy von der Locht</h3>
+            <ul class="list-unstyled list-spacious">
+              <li><span style="color:#888;">Geb.</span> 7.3.1964</li>
+              <li>Prädikatsexamen Informatik an der TU München. Danach Aufbau und Leitung mehrerer Unternehmen, unter anderem als CEO zweier börsennotierter AGs. Seit 2012 als Senior Partner in der E-P-M GmbH.</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+<?php epm_footer(); ?>
--- unternehmen.php
+++ unternehmen.php
@@ -19,16 +19,23 @@
 
     <div class="global-body">
       <div class="container">
+        <h1>Das Unternehmen</h1>
         <div class="row">
-          <div class="col-sm-6">
-            <img class="illustration" src="/img/table-1.jpg" alt="Illustration">
-            <h2>EPM is looking for ambitious people to join their team.</h2>
-            <p>Proin ut urna. Donec ultricies nunc dapibus justo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent vulputate, lectus pulvinar nonummy eleifend, sapien urna posuere metus, vel auctor risus odio eu augue. Cras vitae dolor. Phasellus dolor. Etiam enim. Donec erat felis, tincidunt quis, luctus in, faucibus at, est. <a href="#">Read on &hellip;</a></p>
+          <div class="col-sm-7">
+            <p class="lead">Die EPM GmbH ist seit mehr als 12 Jahren als Dienstleister für die produzierende Industrie tätig. </p>
+
+            <p>In den Anfangsjahren wird das Unternehmen mit dem Aufbau kompletter Produktionsstätten, speziell für Autozulieferer, beauftragt. Von der Teilekalkulation bis hin zur Werksplanung und der anschließenden Inbetriebnahme stand die EPM in der Verantwortung. Heute wird dieses Know-How eingesetzt, um Kunden bei der Erschließung neuer Standorte zu unterstützen.</p>
+
+            <p>Aufgrund eines immer härteren Wettbewerbes und dem damit verbundenen Preisdruck in der Automobil-Branche stieg ab 2006 die Nachfrage im Bereich Restrukturierung und Optimierung bzw. Turnaround-Management, so dass die EPM ihr Angebot weiter ausbauen konnte.</p>
+
+            <p>Nach Abschluss vieler erfolgreicher Restrukturierungsprojekte wurden auch internationale Anfragen bedient. </p>
+
+            <p>Durch das über Jahre aufgebaute Branchenwissen steht auch heute die Automotive-Branche im Mittelpunkt der Beratungsaktivitäten.</p>
+
           </div>
-          <div class="col-sm-6">
-            <img class="illustration" src="/img/table-2.jpg" alt="Illustration">
-            <h2>Super-charge your company with our cool service.</h2>
-            <p>Phasellus dictum blandit tortor. Vivamus a lectus. Ut ligula nibh, sodales pellentesque, lacinia sed, luctus at, ipsum. Curabitur ornare. Sed magna erat, sagittis eget, volutpat sit amet, sodales nec, quam. In lobortis porttitor odio. Donec nunc. Donec tristique ornare eros. Mauris id urna et augue facilisis dictum. <a href="#">Read on &hellip;</a></p>
+          <div class="col-sm-5">
+            <img src="/img/referenz-regionen.png" alt="Referenz regionen">
+            <p style="padding-top:1em;text-align:center;color:#888;">Referenzregionen</p>
           </div>
         </div>
       </div>
